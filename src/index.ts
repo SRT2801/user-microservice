8fc7ae0bab9acbfdfc5e346668da76a6293a093b
SRT2801 <sahirruiz4@gmail.com>
2025-03-07 18:32:32 -0500

    feat: agregar configuración de dotenv y mejorar el manejo del puerto del servidor

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
 import "reflect-metadata";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import { InversifyExpressServer } from "inversify-express-utils";
 import cors from "cors";
 import bodyParser from "body-parser";
-import "./interfaces/controllers/UserController";
-import { initializeContainer } from "./infrastructure/shared/di/container";
+import { initializeContainer } from "./infrastructure/inversify/di/container";
+import { ENVIROMENTS } from "./infrastructure/enviroments/enviroments";
+
+
+import "./infrastructure/http/controllers/UserController";
+import "./infrastructure/http/controllers/AuthController";
 
 async function bootstrap() {
-    try {
-        const container = await initializeContainer();
-        
-        const server = new InversifyExpressServer(container);
+  try {
+   
+    const container = await initializeContainer();
+    const server = new InversifyExpressServer(container);
 
-        server.setConfig((app) => {
-            app.use(cors());
-            app.use(bodyParser.json());
-        });
+    server.setConfig((app) => {
+      app.use(cors());
+      app.use(bodyParser.json());
+    });
 
-        const app = server.build();
-        app.listen(3000, () => {
-            console.log("Server running on port 3000");
-        });
-    } catch (error) {
-        console.error("Error starting server:", error);
-        process.exit(1);
-    }
+    const port = ENVIROMENTS.SERVER_PORT || 3000;
+    const app = server.build();
+
+    app.listen(port, () => {
+      console.log(`Servidor ejecutándose en el puerto ${port}`);
+    });
+  } catch (error) {
+    console.error("Error al iniciar el servidor:", error);
+    process.exit(1);
+  }
 }
 
 bootstrap();
