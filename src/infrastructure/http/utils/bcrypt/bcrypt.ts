6621894d879450043fbec910758045d0b4e27273
SRT2801 <sahirruiz4@gmail.com>
2025-03-07 18:32:18 -0500

    feat: agregar adaptador Bcrypt para el hash y comparación de contraseñas

---
+++ src/infrastructure/http/utils/bcrypt/bcrypt.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from "bcrypt";
+import { injectable } from "inversify";
+
+@injectable()
+export class BcryptAdapter {
+  private readonly saltRounds = 10;
+
+  async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, this.saltRounds);
+  }
+
+  async compare(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
+  }
+}
